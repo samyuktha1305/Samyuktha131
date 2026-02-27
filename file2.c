0309c9d67f0504138bcd4beebb660ebd9f32ed45
Gowri56 <245123748056@mvsrec.edu.in>
2026-02-27 13:19:37 +0530

    text2

--- file2.c
+++ file2.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-int main() {
-    int a, b, sum;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b); // Takes input from the user
-    sum = a + b;
-    printf("Sum = %d", sum); // Prints the sum
-    return 0;
-}
-
+hello
